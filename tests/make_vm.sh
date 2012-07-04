4cd414096fbba5880731a3c863e89334f240f54a
Michel Pelletier <pelletier.michel@yahoo.com>
2012-07-03 23:32:44 -0700

    Some kernel config tweaks to support squashfs for package management, add screen in to make running userland tools easier.

--- tests/make_vm.sh
+++ tests/make_vm.sh
@@ -1,4 +1,3 @@
-CORE64="http://distro.ibiblio.org/tinycorelinux/4.x/x86/release/distribution_files/core64.gz"
 HERE=$PWD
 BUILD_DIR="$PWD/build"
 DISPLAY="0:0"
@@ -23,19 +22,48 @@ fi
 
 if [ ! -e core64.gz ]
 then
-    wget $CORE64
+    wget http://distro.ibiblio.org/tinycorelinux/4.x/x86/release/distribution_files/core64.gz
+    wget http://distro.ibiblio.org/tinycorelinux/4.x/x86/tcz/ncurses-common.tcz
+    wget http://distro.ibiblio.org/tinycorelinux/4.x/x86/tcz/ncurses.tcz
+    wget http://distro.ibiblio.org/tinycorelinux/4.x/x86/tcz/screen.tcz
 fi
+
+unsquashfs -d ncc ncurses-common.tcz
+unsquashfs -d nc ncurses.tcz
+unsquashfs -d sc screen.tcz
 
 cd $BUILD_DIR
 zcat ../core64.gz | sudo cpio -i -H newc -d
+sudo cp -R ../ncc/* .
+sudo cp -R ../nc/* .
+sudo cp -R ../sc/* .
 
 cd $KERNEL
 if [ ! -e .config ]
 then
+    make clean
+    make mrproper
     make defconfig
-    echo "CONFIG_SP=y" >> .config
-    echo "CONFIG_SQUASHFS=y" >> .config
+    echo "
+CONFIG_SP=y
+CONFIG_SQUASHFS=y
+CONFIG_SQUASHFS_XATTR=y
+CONFIG_SQUASHFS_ZLIB=y
+CONFIG_SQUASHFS_LZO=y
+CONFIG_SQUASHFS_XZ=y
+CONFIG_SQUASHFS_4K_DEVBLK_SIZE=y
+CONFIG_SQUASHFS_EMBEDDED=y
+CONFIG_SQUASHFS_FRAGMENT_CACHE_SIZE=3
+CONFIG_KGDB=y
+CONFIG_KGDB_SERIAL_CONSOLE=y
+CONFIG_KGDB_TESTS=n
+CONFIG_KGDB_LOW_LEVEL_TRAP=n
+CONFIG_KGDB_KDB=y
+CONFIG_KDB_KEYBOARD=y
+CONFIG_HAVE_ARCH_KMEMCHECK=y
+" >> .config
 fi
+
 make -j8 bzImage
 make modules
 sudo make INSTALL_MOD_PATH=$BUILD_DIR modules_install firmware_install
@@ -48,7 +76,8 @@ cd ../..
 make clean
 make LDFLAGS=-static
 cd $BUILD_DIR
-sudo cp ../../* home/tc/sp-userland
+sudo cp ../../*_server home/tc/sp-userland
+sudo cp ../../*_client home/tc/sp-userland
 
 sudo find | sudo cpio -o -H newc | gzip -2 > ../spcore64.gz
 cd $HERE
